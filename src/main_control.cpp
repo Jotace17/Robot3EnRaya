031baf746a63150808a01455f089d100c0fb08f1
Lea Rudner <UO302973@uniovi.es>
2024-04-26 13:23:17 +0200

    Standalone Control PID (values currently for motor of forearm) w/ limitation of duty cycle between 0.11 and 0.4. Reference can be put in via serial port once previous angle is reached. Allowed error currently at 1°. Code contains serial printouts which are used for calibration and debugging of the code, can be removed before integrating code.

--- src/main_control.cpp
+++ src/main_control.cpp
@@ -24,7 +24,8 @@
 // Define pins
 int cs_pins[] = {PIN_CS_M1, PIN_CS_M2, PIN_CS_M3};
 
-#define PWM_FREQ_HZ 200
+// defintion of PWM frecuency
+#define PWM_FREQ_HZ 20000
 
 // definition of global variables control
 AlMar::Esp32::Driver_L298n *_m1;
@@ -34,7 +35,7 @@ int _sat = 0;
 
 float _oldRef = 0;
 float _oldAngle = 0;
-float newRef;
+float _newRef;
 
 static int _first_time = 1;
 
@@ -59,37 +60,39 @@ void setup()
 
 void loop()
 {
-  float ducy0;    // variable to store duty cycle
+  // definition of local variables
+  float ducy0;              // variable to store duty cycle
+  float refPos;             // variable for reference angle of control
+  float allowedError = 1.0; // variable to define allowed error of control
 
-  float out;
-  float refPos;
-  if (_first_time) // equal to first_time != 0
+  if (_first_time) // only execute in first execution of loop
   {
-    // currentAngle = GetAngle();
-    // currentAngle = 0;
-    // Serial.printf("initial current angle set to: %.1f", currentAngle);
-    newRef = 50;
+    _newRef = 20;  // first reference at startup currently set to 20° to be defined with calibration 
     _first_time = 0;
   }
 
-  float currentAngle = GetAngle();
+  float currentAngle = GetAngle();    // read angle from encoder
 
-  if (abs(newRef - currentAngle) > 3)
+  if (abs(_newRef - currentAngle) > allowedError)   // control loop if angle difference is > allowed error
   {
-    float refdif = newRef - currentAngle;
-    Serial.printf("newRef-currentAngle = %.2f\n", refdif);
-    out = ControlPid(newRef, _oldRef, currentAngle, _oldAngle);
-    Serial.printf("Control Out: %f.1\n", out);
-    // if (GetAngle()!=NULL)
-    //{
-    _oldAngle = currentAngle;
-    // currentAngle = GetAngle();
-    Serial.printf("deg: %f\t\t### \n", currentAngle);
+    /* only for debugging purposes */
+    float refdif = _newRef - currentAngle;
+    Serial.printf("_newRef-currentAngle = %.2f\t", refdif);
+    
+    /* calculation of duty cycle by control */
+    float ducy_m1 = ControlPid(_newRef, _oldRef, currentAngle, _oldAngle);
+    Serial.printf("Control Out: %f.1  \t\t", ducy_m1);
 
-    //float ducy_m1 = (abs(out) / 255);
-    float ducy_m1 = out;
-    Serial.printf("ducy: %.2f\n", ducy_m1);
-    if (abs(ducy_m1) < 0.003  )  // motor 877-7165 is not moving with dutycycle lower than 0.11
+    _oldAngle = currentAngle;         // update value of old angle 
+
+    /* only for debugging purposes */
+    Serial.printf("Current Angle deg: %f\t\t### \t\t", currentAngle);
+    Serial.printf("ducy: %.2f \t", ducy_m1);
+    Serial.printf("_newRef: %f \n", _newRef);
+
+    /* limitation of duty cycle */
+    /* to be cleaned up and put into function or into control */
+    if (abs(ducy_m1) < 0.001) // motor 877-7174 is not moving with dutycycle lower than 0.11
     {
       _m1->SetDuty(0.0);
     }
@@ -97,7 +100,7 @@ void loop()
     {
       _m1->SetDuty(0.11);
     }
-    else if ((ducy_m1) > -0.11 && ducy_m1 <-0.003)
+    else if ((ducy_m1) > -0.11 && ducy_m1 < -0.001)
     {
       _m1->SetDuty(-0.11);
     }
@@ -105,7 +108,7 @@ void loop()
     {
       _m1->SetDuty(0.4);
     }
-     else if ((ducy_m1) < -0.4)
+    else if ((ducy_m1) < -0.4)
     {
       _m1->SetDuty(-0.4);
     }
@@ -116,116 +119,94 @@ void loop()
   }
   else
   {
-    _m1->SetDuty(0);            // set ducy cycle 0 to stop motor, as end position had ben reached 
-    Serial.printf("else\n");
+    /* stop motor and read actual angle */
+    _m1->SetDuty(0); 
     currentAngle = GetAngle();
     Serial.printf("else: angle read %.2f:\n", currentAngle);
-    // delay(400);
-    //newRef = GetReference();
-    // newRef = currentAngle + 3;
-  }
-  float _oldRef = newRef;
 
-  delay(1);
+    /* reading of new reference position */
+    /* currently does only work in loop, because function returns 0 if there is no input in serial content is the same as in GetReference() */
+    String refPosition;
+    Serial.printf("Put in desired position in degree: \n");
+    while (Serial.available() > 0)
+    {
+      /* Read the input position */
+      refPosition = Serial.readString();
+      refPosition.trim();
+      Serial.printf("ref set to: %f \n", refPosition.toFloat());
+      _newRef = refPosition.toFloat();      // pass string to float value for control
+    }
+  }
+  float _oldRef = _newRef;          // update value of previous reference - tbc if it is the right place here?? 
+
+  /* wait 100ms */
+  /* only for standalone control code necessary to be done by scheduler in integrated program */
+  delay(100);
 }
 
-// put function definitions here:#
+/* function definitions */
 float GetAngle()
 {
   // read encoder
   int pos = _enc->Read(0);
   float posF = (float)pos;
-  // int pos2 = _enc->Read(1); // lee encoder 0 (M1)
 
   if (pos != 0x80000000)
   {
-    // Serial.printf("MOTOR %i \t\t ### \t\t\t MOTOR %i\n", 0, 1);
     float readAngle = posF * 360 / 4096;
-    // Serial.printf("deg: %f, read: 0x%08x \t\t### \n", readAngle, pos);
     return readAngle;
   }
   else
   {
-    // Serial.printf("\t ### ERROR LECTURA: %lu\n", pos);
+    // do nothing
   }
-  //delay(20);
 }
 
 float GetReference()
 {
   float refPos;
   String refPosition;
+  Serial.printf("Put in desired position in degree: \n");
 
-  if (Serial.available() > 0)
+  while (Serial.available() > 0)
   {
     // Read the input position
-    Serial.printf("Put in desired position in degree: \n");
+
     refPosition = Serial.readString();
-    Serial.printf("ref position: %s", refPosition);
+    refPosition.trim();
+    // Serial.printf("ref position: %s", refPosition);
+    Serial.printf("ref set to: %f \n", refPosition.toFloat());
+
+    refPos = refPosition.toFloat();
   }
-  delay(400);
-  return refPos = refPosition.toFloat();
+  return refPos;
 }
 
 float ControlPid(float ref, float refOld, float angle, float angleOld)
 {
   // defintion of constants of control
-  // those values will be updated with the values from PID tuning with the Simulink Model which provided Juan
-  float kP = 0.8; // proportional gain
-  float kI = 0;   // integral gain
-  float kD = 0; // differential gain
+  float kP = 2.9;  // proportional gain - motor forearm
+  float kI = 0.22; // integral gain - motor forearm
+  float kD = 0.1;  // differential gain -motor forearm
 
   // definition of timestep (depends on recurring task time in which control will run) & saturation limits
   // for now set to 10ms
-  float timeStep = 0.001;
+  float timeStep = 0.1;
 
   // definition of factors for easier readability of the formulas, only needed if filter will be used
   float intPart;
 
   // calculate integral part
-  /*float dI = kI * timeStep * (refOld - angleOld);
-  // Antiwind-up
-  if (_sat * dI > 0)
-  {
-    intPart = _iOld;
-  }
-  else
-  {
-    intPart = _iOld + dI;
-  }*/
+  float dI = kI * timeStep * ((refOld - angleOld) / 360);
 
   // calculate proportional part
-  float propPart = kP * ((ref - angle)/360);
+  float propPart = kP * ((ref - angle) / 360);
 
   // calculate differential part
-  float difPart = _dOld + kD * ((ref - refOld)/360) - kD * ((angle - angleOld)/360);
+  float difPart = _dOld + kD * ((ref - refOld) / 360) - kD * ((angle - angleOld) / 360);
 
   // calculate control output
-  //float controlOut = -(propPart + intPart + difPart);
-  float controlOut = (propPart+difPart);
-  // limitation of output signal
-  /*if (controlOut > satPos)
-  {
-    controlOut = satPos;
-    _sat = 1;
-  }
-  else if (controlOut < satNeg)
-  {
-    controlOut = satNeg;
-    _sat = -1;
-  }
-  else
-  {
-    _iOld = intPart; // update integral for next step
-  }
-  _dOld = difPart; // update difPart for next step*/
-  /*if (abs(controlOut) < 1)
-  {
-    return 0;
-  }
-  else
-  {
-    return controlOut;
-  }*/
+  float controlOut = (propPart + difPart + intPart);
+
   return controlOut;
 }
